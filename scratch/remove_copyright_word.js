fee35cfeb99fd43436d7c42a920ede9e5182cbc3
AdarshJha05 <adarshj595@gmail.com>
2026-09-16 00:28:06 +0530

    fix: remove redundant 'Copyright' word from footer

---
+++ scratch/remove_copyright_word.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const path = require('path');
+
+const root = path.resolve(__dirname, '..');
+
+function getHtmlFiles(dir) {
+  let results = [];
+  const list = fs.readdirSync(dir);
+  list.forEach(function(file) {
+    const fullPath = path.join(dir, file);
+    const stat = fs.statSync(fullPath);
+    if (stat && stat.isDirectory()) {
+      if (!fullPath.includes('node_modules') && !fullPath.includes('.git') && !fullPath.includes('nextjs-app')) {
+        results = results.concat(getHtmlFiles(fullPath));
+      }
+    } else {
+      if (fullPath.endsWith('.html') && !fullPath.includes('scratch')) {
+        results.push(fullPath);
+      }
+    }
+  });
+  return results;
+}
+
+const htmlFiles = getHtmlFiles(root);
+let modifiedCount = 0;
+
+htmlFiles.forEach(file => {
+  let content = fs.readFileSync(file, 'utf8');
+  let changed = false;
+
+  const oldText = 'Copyright &copy; 2026 Sahapathika.';
+  const newText = '&copy; 2026 Sahapathika.';
+
+  if (content.includes(oldText)) {
+    content = content.replace(new RegExp(oldText.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&'), 'g'), newText);
+    changed = true;
+  }
+
+  if (changed) {
+    fs.writeFileSync(file, content, 'utf8');
+    modifiedCount++;
+  }
+});
+
+console.log('Modified files:', modifiedCount);
